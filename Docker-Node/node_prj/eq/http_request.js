681f9383fd5e437f71db351c2b52a617c91935cd
三上幸太 <mikami@groxi.jp>
2022-12-23 15:48:31 +0900

    地震課題の回答

---
+++ Docker-Node/node_prj/eq/http_request.js
@@ -0,0 +1,40 @@
+// ごく軽微な地震や震源を特定出来ない情報の取り扱い。マグニチュードが負数となる
+
+const getUser = async (userId) => {
+    const url = `https://api.p2pquake.net/v2/jma/quake?limit=100`;
+    const response = await fetch(url, { method: "get"})
+    const json = response.json();
+    if (response.status == 200) {
+      return Promise.resolve(json);
+    } else {
+      return Promise.reject(json.error);
+    }
+  };
+
+getUser(10)
+.then((data) => {
+    console.log(data);
+    const getEQlist = data.map(function( value, index, array ) {
+      //if (value['earthquake']['hypocenter']['magnitude'] > 0) {
+        return { 'cr':value['created_at'], 'magnitude':value['earthquake']['hypocenter']['magnitude'], 'hypocenter':value['earthquake']['hypocenter']['name'] };
+      //}
+    });
+
+    // CSV
+    const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+    const csvWriter = createCsvWriter({
+    path: 'data.csv',       // 保存する先のパス(すでにファイルがある場合は上書き保存)
+    header: ['cr', 'magnitude', 'hypocenter']  // 出力する項目(ここにない項目はスキップされる)
+    });
+
+    // 書き込み
+    csvWriter.writeRecords(getEQlist)
+    .then(() => {
+        console.log('done');
+    });
+
+    //console.log(getEQlist);
+})
+.catch((err) => {
+    console.log(err);
+});
---
+++ Docker-Node/node_prj/sample/json2.js
@@ -0,0 +1,64 @@
+var objJson = [
+    {"code":551,"created_at":"2022/12/23 01:43:39.756","earthquake":{"domesticTsunami":"None","foreignTsunami":"Unknown","hypocenter":{"depth":10,"latitude":41.6,"longitude":140.6,"magnitude":3.8,"name":"津軽海峡"},"maxScale":20,"time":"2022/12/23 01:40:00"},"id":"63a4893bf0f6de0007946ceb","issue":{"correct":"None","source":"気象庁","time":"2022/12/23 01:43:07","type":"DetailScale"},"points":[{"addr":"函館市大森町","isArea":false,"pref":"北海道","scale":20},{"addr":"函館市泊町","isArea":false,"pref":"北海道","scale":20},
+    {"addr":"函館市川汲町","isArea":false,"pref":"北海道","scale":20},{"addr":"函館市美原","isArea":false,"pref":"北海道","scale":10},{"addr":"七飯町本町","isArea":false,"pref":"北海道","scale":20},{"addr":"七飯町桜町","isArea":false,"pref":"北海道","scale":10},
+    {"addr":"渡島北斗市中央","isArea":false,"pref":"北海道","scale":10},{"addr":"渡島北斗市本町","isArea":false,"pref":"北海道","scale":10},{"addr":"渡島森町上台町","isArea":false,"pref":"北海道","scale":10},{"addr":"知内町重内","isArea":false,"pref":"北海道","scale":20},
+    {"addr":"知内町小谷石","isArea":false,"pref":"北海道","scale":10},{"addr":"木古内町木古内","isArea":false,"pref":"北海道","scale":10},{"addr":"八雲町熊石雲石町","isArea":false,"pref":"北海道","scale":10},{"addr":"大間町大間","isArea":false,"pref":"青森県","scale":10},
+    {"addr":"佐井村長後","isArea":false,"pref":"青森県","scale":10},{"addr":"佐井村佐井","isArea":false,"pref":"青森県","scale":10}],"time":"2022/12/23 01:43:07.899","timestamp":{"convert":"2022/12/23 01:43:07.893","register":"2022/12/23 01:43:07.899"},
+    "user_agent":"jmaxml-seis-parser-go, relay, register-api","ver":"20220813"},
+    {"code":551,"created_at":"2022/12/22 19:31:51.566","earthquake":{"domesticTsunami":"None","foreignTsunami":"Unknown","hypocenter":{"depth":90,"latitude":35.7,"longitude":140,
+    "magnitude":3.3,"name":"千葉県北西部"},"maxScale":10,"time":"2022/12/22 19:28:00"},"id":"63a43217f0f6de0007946c91","issue":{"correct":"None","source":"気象庁","time":"2022/12/22 19:31:40","type":"DetailScale"},"points":[{"addr":"千葉花見川区花島町",
+    "isArea":false,"pref":"千葉県","scale":10},{"addr":"千葉稲毛区園生町","isArea":false,"pref":"千葉県","scale":10}],"time":"2022/12/22 19:31:41.194","timestamp":{"convert":"2022/12/22 19:31:41.19","register":"2022/12/22 19:31:41.195"},
+    "user_agent":"jmaxml-seis-parser-go, relay, register-api","ver":"20220813"},
+    {"code":551,"created_at":"2022/12/22 18:52:16.064","earthquake":
+        {"domesticTsunami":"None","foreignTsunami":"Unknown","hypocenter":
+            {"depth":70,"latitude":32.8,"longitude":131.5,"magnitude":3,"name":"宮崎県北部山沿い"},
+            "maxScale":10,"time":"2022/12/22 18:48:00"},
+            "id":"63a428d0f0f6de0007946c73",
+            "issue":{"correct":"None","source":"気象庁","time":"2022/12/22 18:51:44","type":"DetailScale"},
+                "points":[
+                    {"addr":"佐伯市蒲江蒲江浦","isArea":false,"pref":"大分県","scale":10},
+                    {"addr":"佐伯市宇目","isArea":false,"pref":"大分県","scale":10},{"addr":"延岡市北川町川内名白石","isArea":false,"pref":"宮崎県","scale":10},
+                    {"addr":"高千穂町三田井","isArea":false,"pref":"宮崎県","scale":10},{"addr":"高千穂町寺迫","isArea":false,"pref":"宮崎県","scale":10}],
+            "time":"2022/12/22 18:51:44.958","timestamp":{"convert":"2022/12/22 18:51:44.954","register":"2022/12/22 18:51:44.958"
+        },
+    "user_agent":"jmaxml-seis-parser-go, relay, register-api","ver":"20220813"},
+    {"code":551,"created_at":"2022/12/22 17:47:26.352","earthquake":{"domesticTsunami":"None","foreignTsunami":"Unknown","hypocenter":{"depth":10,"latitude":35.1,"longitude":140.2,"magnitude":3.2,"name":"千葉県南東沖"},"maxScale":10,"time":"2022/12/22 17:44:00"},"id":"63a4199ef0f6de0007946c61","issue":{"correct":"None","source":"気象庁","time":"2022/12/22 17:46:53","type":"DetailScale"},"points":[{"addr":"長南町総合グラウンド","isArea":false,"pref":"千葉県","scale":10},{"addr":"勝浦市墨名","isArea":false,"pref":"千葉県","scale":10},{"addr":"勝浦市新官","isArea":false,"pref":"千葉県","scale":10},{"addr":"鴨川市八色","isArea":false,"pref":"千葉県","scale":10},{"addr":"鴨川市横渚","isArea":false,"pref":"千葉県","scale":10},{"addr":"鴨川市天津","isArea":false,"pref":"千葉県","scale":10},{"addr":"君津市久留里市場","isArea":false,"pref":"千葉県","scale":10},{"addr":"大多喜町大多喜","isArea":false,"pref":"千葉県","scale":10}],"time":"2022/12/22 17:46:54.154","timestamp":{"convert":"2022/12/22 17:46:54.137","register":"2022/12/22 17:46:54.154"},"user_agent":"jmaxml-seis-parser-go, relay, register-api","ver":"20220813"},
+    {"code":551,"created_at":"2022/12/22 17:39:24.229","earthquake":{"domesticTsunami":"None","foreignTsunami":"Unknown","hypocenter":{"depth":20,"latitude":35.1,"longitude":140.2,"magnitude":2.9,"name":"千葉県南東沖"},"maxScale":10,"time":"2022/12/22 17:36:00"},"id":"63a417bcf0f6de0007946c57","issue":{"correct":"None","source":"気象庁","time":"2022/12/22 17:38:50","type":"DetailScale"},"points":[{"addr":"鴨川市八色","isArea":false,"pref":"千葉県","scale":10},{"addr":"鴨川市横渚","isArea":false,"pref":"千葉県","scale":10},{"addr":"鴨川市天津","isArea":false,"pref":"千葉県","scale":10},{"addr":"君津市久留里市場","isArea":false,"pref":"千葉県","scale":10}],"time":"2022/12/22 17:38:50.776","timestamp":{"convert":"2022/12/22 17:38:50.772","register":"2022/12/22 17:38:50.776"},"user_agent":"jmaxml-seis-parser-go, relay, register-api","ver":"20220813"},
+    {"code":551,"created_at":"2022/12/22 17:32:21.125","earthquake":{"domesticTsunami":"None","foreignTsunami":"Unknown","hypocenter":{"depth":20,"latitude":35.1,"longitude":140.2,"magnitude":3,"name":"千葉県南東沖"},"maxScale":10,"time":"2022/12/22 17:28:00"},"id":"63a41615f0f6de0007946c51","issue":{"correct":"None","source":"気象庁","time":"2022/12/22 17:31:49","type":"DetailScale"},"points":[{"addr":"長南町総合グラウンド","isArea":false,"pref":"千葉県","scale":10},{"addr":"長南町長南","isArea":false,"pref":"千葉県","scale":10},{"addr":"鴨川市八色","isArea":false,"pref":"千葉県","scale":10},{"addr":"鴨川市横渚","isArea":false,"pref":"千葉県","scale":10},{"addr":"鴨川市天津","isArea":false,"pref":"千葉県","scale":10},{"addr":"君津市久留里市場","isArea":false,"pref":"千葉県","scale":10},{"addr":"大多喜町大多喜","isArea":false,"pref":"千葉県","scale":10}],"time":"2022/12/22 17:31:50.176","timestamp":{"convert":"2022/12/22 17:31:50.172","register":"2022/12/22 17:31:50.176"},"user_agent":"jmaxml-seis-parser-go, relay, register-api","ver":"20220813"},
+    {"code":551,"created_at":"2022/12/22 10:53:22.671","earthquake":{"domesticTsunami":"None","foreignTsunami":"Unknown","hypocenter":{"depth":10,"latitude":32.7,"longitude":130.1,"magnitude":2.8,"name":"橘湾"},"maxScale":10,"time":"2022/12/22 10:49:00"},"id":"63a3b892f0f6de0007946bfa","issue":{"correct":"None","source":"気象庁","time":"2022/12/22 10:52:51","type":"DetailScale"},"points":[{"addr":"長崎市元町","isArea":false,"pref":"長崎県","scale":10},{"addr":"諫早市多良見町","isArea":false,"pref":"長崎県","scale":10},{"addr":"雲仙市南串山町","isArea":false,"pref":"長崎県","scale":10},{"addr":"南島原市加津佐町","isArea":false,"pref":"長崎県","scale":10}],"time":"2022/12/22 10:52:51.845","timestamp":{"convert":"2022/12/22 10:52:51.841","register":"2022/12/22 10:52:51.845"},"user_agent":"jmaxml-seis-parser-go, relay, register-api","ver":"20220813"},
+    {"code":551,"created_at":"2022/12/22 10:47:17.581","earthquake":{"domesticTsunami":"None","foreignTsunami":"Unknown","hypocenter":{"depth":10,"latitude":32.7,"longitude":130.1,"magnitude":3.4,"name":"橘湾"},"maxScale":20,"time":"2022/12/22 10:43:00"},"id":"63a3b725f0f6de0007946bf5","issue":{"correct":"None","source":"気象庁","time":"2022/12/22 10:46:45","type":"DetailScale"},"points":[{"addr":"長崎市元町","isArea":false,"pref":"長崎県","scale":20},{"addr":"諫早市多良見町","isArea":false,"pref":"長崎県","scale":20},{"addr":"諫早市東小路町","isArea":false,"pref":"長崎県","scale":10},{"addr":"諫早市堂崎町","isArea":false,"pref":"長崎県","scale":10},{"addr":"諫早市飯盛町","isArea":false,"pref":"長崎県","scale":10},{"addr":"諫早市森山町","isArea":false,"pref":"長崎県","scale":10},{"addr":"大村市玖島","isArea":false,"pref":"長崎県","scale":10},{"addr":"時津町浦","isArea":false,"pref":"長崎県","scale":10},{"addr":"雲仙市小浜町雲仙","isArea":false,"pref":"長崎県","scale":20},{"addr":"雲仙市南串山町","isArea":false,"pref":"長崎県","scale":20},{"addr":"雲仙市国見町","isArea":false,"pref":"長崎県","scale":10},{"addr":"雲仙市雲仙出張所","isArea":false,"pref":"長崎県","scale":10},{"addr":"雲仙市小浜町北本町","isArea":false,"pref":"長崎県","scale":10},{"addr":"南島原市口之津町","isArea":false,"pref":"長崎県","scale":20},{"addr":"南島原市加津佐町","isArea":false,"pref":"長崎県","scale":20},{"addr":"南島原市南有馬町","isArea":false,"pref":"長崎県","scale":10},{"addr":"南島原市北有馬町","isArea":false,"pref":"長崎県","scale":10},{"addr":"上天草市松島町","isArea":false,"pref":"熊本県","scale":10}],"time":"2022/12/22 10:46:46.31","timestamp":{"convert":"2022/12/22 10:46:46.3","register":"2022/12/22 10:46:46.31"},"user_agent":"jmaxml-seis-parser-go, relay, register-api","ver":"20220813"},
+    {"code":551,"created_at":"2022/12/22 10:30:33.369","earthquake":{"domesticTsunami":"None","foreignTsunami":"Unknown","hypocenter":{"depth":10,"latitude":32.3,"longitude":130.5,"magnitude":2.2,"name":"熊本県天草・芦北地方"},"maxScale":10,"time":"2022/12/22 10:26:00"},"id":"63a3b339f0f6de0007946bed","issue":{"correct":"None","source":"気象庁","time":"2022/12/22 10:30:07","type":"DetailScale"},"points":[{"addr":"芦北町田浦町","isArea":false,"pref":"熊本県","scale":10}],"time":"2022/12/22 10:30:09.142","timestamp":{"convert":"2022/12/22 10:30:09.136","register":"2022/12/22 10:30:09.142"},"user_agent":"jmaxml-seis-parser-go, relay, register-api","ver":"20220813"},
+    {"code":551,"created_at":"2022/12/21 22:30:34.519","earthquake":{"domesticTsunami":"None","foreignTsunami":"Unknown","hypocenter":{"depth":10,"latitude":34.2,"longitude":135.5,"magnitude":3.3,"name":"和歌山県北部"},"maxScale":20,"time":"2022/12/21 22:27:00"},"id":"63a30a7af0f6de0007946b4f","issue":{"correct":"None","source":"気象庁","time":"2022/12/21 22:30:19","type":"DetailScale"},"points":[{"addr":"橋本市東家","isArea":false,"pref":"和歌山県","scale":20},{"addr":"橋本市高野口町名倉","isArea":false,"pref":"和歌山県","scale":10},{"addr":"紀の川市粉河","isArea":false,"pref":"和歌山県","scale":20},{"addr":"紀の川市那賀総合センター","isArea":false,"pref":"和歌山県","scale":20},{"addr":"紀の川市西大井","isArea":false,"pref":"和歌山県","scale":10},{"addr":"紀の川市桃山町元","isArea":false,"pref":"和歌山県","scale":10},{"addr":"かつらぎ町丁ノ町","isArea":false,"pref":"和歌山県","scale":20},{"addr":"紀美野町下佐々","isArea":false,"pref":"和歌山県","scale":10},{"addr":"九度山町九度山","isArea":false,"pref":"和歌山県","scale":10},{"addr":"高野町高野山中学校","isArea":false,"pref":"和歌山県","scale":10},{"addr":"高野町役場","isArea":false,"pref":"和歌山県","scale":10},{"addr":"泉佐野市市場","isArea":false,"pref":"大阪府","scale":10},{"addr":"熊取町野田","isArea":false,"pref":"大阪府","scale":10},{"addr":"五條市岡口","isArea":false,"pref":"奈良県","scale":10},{"addr":"高取町観覚寺","isArea":false,"pref":"奈良県","scale":10},{"addr":"吉野町上市","isArea":false,"pref":"奈良県","scale":10}],"time":"2022/12/21 22:30:20.324","timestamp":{"convert":"2022/12/21 22:30:20.3","register":"2022/12/21 22:30:20.324"},"user_agent":"jmaxml-seis-parser-go, relay, register-api","ver":"20220813"}];
+
+/*
+for (item of objJson) {
+    console.log (item['earthquake']['hypocenter']['name']);
+}
+*/
+
+// scaleが20以上の地点を出力してみる
+var data = objJson[0]['points'];
+let count=0;
+for (item of data) {
+    let scale = item['scale'];
+    let addr = item['addr'];
+
+    if (scale >= 20) {
+        console.log (addr + ':' + scale);
+        count++;
+    }
+}
+console.log ('地点数:' + count);
+
+// scaleが20以上の地点を出力してみる(map編)
+var data = objJson[0]['points'];
+const test = data.map(function( value, index, array ) {
+    if (value['scale'] >= 20) {
+        return value;
+    }
+}).filter(v => v);
+console.log (test.length);
+
+const aa = test.map(function (value) {
+    console.log (value['pref']);
+});
